e3524bd7a72ecea97e492f2d219f58efdf0dc48b
Antonio Pirrello <a.pirrello@logicalsystem.it>
2023-06-27 17:35:12 +0200

    fix Autodeployer

--- Compile-PS.ps1
+++ Compile-PS.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy unrestricted 
 
-.\ALC\alc.exe /project:. /out:.\Olister.app /packagecachepath:.\AlPackages
+#.\ALC\alc.exe /project:. /out:.\Olister.app /packagecachepath:.\AlPackages
 
 ls
 
@@ -15,10 +15,15 @@ $ClientSecret = "~Xk8Q~Uho6Jz3MmdMbX5rlQWnyRu4IflZ_vsZdol"
 $loginURL     = "https://login.microsoftonline.com"
 $tenantId     = "763b2d72-dcd0-44c9-b236-7f57e4eb7c51"
 $scopes       = "https://api.businesscentral.dynamics.com/.default"
-$environment  = "v17"
-$baseUrl      = "https://api.businesscentral.dynamics.com/v2.0/$environment/api/microsoft/automation/v1.0"
+$companyId      = "ab80bd8d-339d-eb11-8ce5-000d3a21fbdf"
+$environment  = "DEV"
+$baseUrl      = "https://api.businesscentral.dynamics.com/v2.0/"+$tenantId+"/"+$environment+"/api/v2.0/companies("+$companyId+")/extensionUpload(0)/content"
 
-# $appFile      = "D:\Repos\Local\AL\ALProject254\Default publisher_ALProject254_1.0.0.0.app"
+#Prepare token request
+$authUrl = 'https://login.microsoftonline.com/' + $tenantId + '/oauth2/v2.0/token'
+
+$baseUrl      = "https://api.businesscentral.dynamics.com/v2.0/$environment/api/microsoft/automation/v1.0"
+$appFile =  (Get-Item (".\Olister.app")).FullName
 # Get access token 
 $body = @{grant_type="client_credentials";scope=$scopes;client_id=$ClientID;client_secret=$ClientSecret}
 $oauth = Invoke-RestMethod -Method Post -Uri $("$loginURL/$tenantId/oauth2/v2.0/token") -Body $body
@@ -28,7 +33,7 @@ $companies = Invoke-RestMethod `
              -Uri $("$baseurl/companies") `
              -Headers @{Authorization='Bearer ' + $oauth.access_token}
 $companies.value | Format-Table -AutoSize
-$companyId = $companies.value[0].id
+$companyId = $companies.value[1].id
 # Upload and install app
 Invoke-RestMethod `
 -Method Patch `
@@ -37,4 +42,4 @@ Invoke-RestMethod `
 -ContentType "application/octet-stream" `
 -InFile $appFile
 
-Start-Sleep -s 10
+#Start-Sleep -s 10
